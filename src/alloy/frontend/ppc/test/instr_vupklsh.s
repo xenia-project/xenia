dd2f799380c28029d22997f770e9ebc63239efec
Ben Vanik <ben.vanik@gmail.com>
2015-01-19 11:52:51 -0800

    Tests for vupklsh/vupkhsh.

---
+++ src/alloy/frontend/ppc/test/instr_vupklsh.s
@@ -0,0 +1,11 @@
+test_vupklsh_0:
+  #_ REGISTER_IN v3 [00010203, 04050607, 08090A0B, 0C0D0E0F]
+  vupklsh v3, v3
+  blr
+  #_ REGISTER_OUT v3 [00000809, 00000a0b, 00000c0d, 00000e0f]
+
+test_vupklsh_1:
+  #_ REGISTER_IN v3 [7F800203, 04050607, 7F800A0B, 0C0D0E0F]
+  vupklsh v3, v3
+  blr
+  #_ REGISTER_OUT v3 [00007f80, 00000a0b, 00000c0d, 00000e0f]
