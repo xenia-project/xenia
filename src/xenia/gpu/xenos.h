de5bad5fb2917504ebf68737138a03ad0fb6711f
Ben Vanik <ben.vanik@gmail.com>
2015-02-13 10:14:28 -0800

    Removing weird GPU masking. Fixes a lot of bad shader loads.

--- src/xenia/gpu/xenos.h
+++ src/xenia/gpu/xenos.h
@@ -238,7 +238,8 @@ inline uint32_t GpuToCpu(uint32_t base, uint32_t p) {
   // Some AMD docs say relative to base ptr, some say just this.
   // Some games use some crazy shift magic, but it seems to nop.
   uint32_t upper = 0;//base & 0xFF000000;
-  uint32_t lower = p & 0x01FFFFFF;
+  //uint32_t lower = p & 0x01FFFFFF;
+  uint32_t lower = p;
   return upper + lower;// -(((base >> 20) + 0x200) & 0x1000);
 }
 
