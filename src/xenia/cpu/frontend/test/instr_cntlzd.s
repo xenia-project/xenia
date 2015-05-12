19e733cff73aff80f8613088b70a36fe7344300a
gibbed <rick@gibbed.us>
2015-05-12 01:19:56 -0500

    Added constant tests for cntlzd and cntlzw.

--- src/xenia/cpu/frontend/test/instr_cntlzd.s
+++ src/xenia/cpu/frontend/test/instr_cntlzd.s
@@ -5,8 +5,22 @@ test_cntlzd_1:
   #_ REGISTER_OUT r5 0
   #_ REGISTER_OUT r6 64
 
+test_cntlzd_1_constant:
+  li r5, 0
+  cntlzd r6, r5
+  blr
+  #_ REGISTER_OUT r5 0
+  #_ REGISTER_OUT r6 64
+
 test_cntlzd_2:
   #_ REGISTER_IN r5 1
+  cntlzd r6, r5
+  blr
+  #_ REGISTER_OUT r5 1
+  #_ REGISTER_OUT r6 63
+
+test_cntlzd_2_constant:
+  li r5, 1
   cntlzd r6, r5
   blr
   #_ REGISTER_OUT r5 1
@@ -19,8 +33,25 @@ test_cntlzd_3:
   #_ REGISTER_OUT r5 0xFFFFFFFFFFFFFFFF
   #_ REGISTER_OUT r6 0
 
+test_cntlzd_3_constant:
+  li r5, 0
+  not r5, r5
+  cntlzd r6, r5
+  blr
+  #_ REGISTER_OUT r5 0xFFFFFFFFFFFFFFFF
+  #_ REGISTER_OUT r6 0
+
 test_cntlzd_4:
   #_ REGISTER_IN r5 0x7FFFFFFFFFFFFFFF
+  cntlzd r6, r5
+  blr
+  #_ REGISTER_OUT r5 0x7FFFFFFFFFFFFFFF
+  #_ REGISTER_OUT r6 1
+
+test_cntlzd_4_constant:
+  li r5, 0
+  not r5, r5
+  srdi r5, r5, 1
   cntlzd r6, r5
   blr
   #_ REGISTER_OUT r5 0x7FFFFFFFFFFFFFFF
--- src/xenia/cpu/frontend/test/instr_cntlzw.s
+++ src/xenia/cpu/frontend/test/instr_cntlzw.s
@@ -5,8 +5,22 @@ test_cntlzw_1:
   #_ REGISTER_OUT r5 0
   #_ REGISTER_OUT r6 32
 
+test_cntlzw_1_constant:
+  li r5, 0
+  cntlzw r6, r5
+  blr
+  #_ REGISTER_OUT r5 0
+  #_ REGISTER_OUT r6 32
+
 test_cntlzw_2:
   #_ REGISTER_IN r5 1
+  cntlzw r6, r5
+  blr
+  #_ REGISTER_OUT r5 1
+  #_ REGISTER_OUT r6 31
+
+test_cntlzw_2_constant:
+  li r5, 1
   cntlzw r6, r5
   blr
   #_ REGISTER_OUT r5 1
@@ -19,8 +33,26 @@ test_cntlzw_3:
   #_ REGISTER_OUT r5 0xFFFFFFFF
   #_ REGISTER_OUT r6 0
 
+test_cntlzw_3_constant:
+  li r5, 0
+  not r5, r5
+  srwi r5, r5, 0
+  cntlzw r6, r5
+  blr
+  #_ REGISTER_OUT r5 0xFFFFFFFF
+  #_ REGISTER_OUT r6 0
+
 test_cntlzw_4:
   #_ REGISTER_IN r5 0x7FFFFFFF
+  cntlzw r6, r5
+  blr
+  #_ REGISTER_OUT r5 0x7FFFFFFF
+  #_ REGISTER_OUT r6 1
+
+test_cntlzw_4_constant:
+  li r5, 0
+  not r5, r5
+  srwi r5, r5, 1
   cntlzw r6, r5
   blr
   #_ REGISTER_OUT r5 0x7FFFFFFF
