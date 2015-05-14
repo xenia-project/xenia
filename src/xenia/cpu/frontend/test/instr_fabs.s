ac4337cabf8a48a9a569b662ecff26c098a4c7e5
gibbed <rick@gibbed.us>
2015-05-14 16:02:44 -0500

    CR1 tests for fabs (disabled).

--- src/xenia/cpu/frontend/test/instr_fabs.s
+++ src/xenia/cpu/frontend/test/instr_fabs.s
@@ -15,3 +15,27 @@ test_fabs_3:
   fabs f1, f1
   blr
   #_ REGISTER_OUT f1 1234.0
+
+#test_fabs_cr_1:
+#  #_ REGISTER_IN f1 1.0
+#  fabs. f1, f1
+#  mfcr r12
+#  blr
+#  #_ REGISTER_OUT f1 1.0
+#  #_ REGISTER_OUT r12 0x04000000
+
+#test_fabs_cr_2:
+#  #_ REGISTER_IN f1 -1.0
+#  fabs. f1, f1
+#  mfcr r12
+#  blr
+#  #_ REGISTER_OUT f1 1.0
+#  #_ REGISTER_OUT r12 0x08000000
+
+#test_fabs_cr_3:
+#  #_ REGISTER_IN f1 -1234.0
+#  fabs. f1, f1
+#  mfcr r12
+#  blr
+#  #_ REGISTER_OUT f1 1234.0
+#  #_ REGISTER_OUT r12 0x08000000
