45cd0e4f8f3d33f7799ee64bb77408e226b6e611
gibbed <rick@gibbed.us>
2015-05-22 01:30:44 -0500

    Adjustments to fmadd tests.

--- src/xenia/cpu/frontend/test/instr_fmadd.s
+++ src/xenia/cpu/frontend/test/instr_fmadd.s
@@ -18,6 +18,9 @@ test_fmadd_2:
   fmadd f1, f2, f3, f4
   blr
   #_ REGISTER_OUT f1 15.0
+  #_ REGISTER_OUT f2 5.0
+  #_ REGISTER_OUT f3 0.0
+  #_ REGISTER_OUT f4 15.0
 
 test_fmadd_3:
   #_ REGISTER_IN f1 0.0
@@ -27,3 +30,18 @@ test_fmadd_3:
   fmadd f1, f2, f3, f4
   blr
   #_ REGISTER_OUT f1 40.0
+  #_ REGISTER_OUT f2 5.0
+  #_ REGISTER_OUT f3 5.0
+  #_ REGISTER_OUT f4 15.0
+
+test_fmadd_4:
+  #_ REGISTER_IN f1 0.0
+  #_ REGISTER_IN f2 9999.99
+  #_ REGISTER_IN f3 9999.99
+  #_ REGISTER_IN f4 9999.99
+  fmadd f1, f2, f3, f4
+  blr
+  #_ REGISTER_OUT f1 100009799.9901
+  #_ REGISTER_OUT f2 9999.99
+  #_ REGISTER_OUT f3 9999.99
+  #_ REGISTER_OUT f4 9999.99
