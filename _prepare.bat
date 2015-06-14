c3c752b1f3066f235f3ec7528db7248be56663ce
Ben Vanik <ben.vanik@gmail.com>
2015-06-13 22:54:50 -0700

    Fixing flicker in Firefox. Fuck IE.

--- _prepare.bat
+++ _prepare.bat
@@ -1,1 +1,6 @@
-vulcanize --inline-css --inline-scripts --strip-comments polymer.html > polymer-vulcanized.html
+CALL vulcanize --inline-css --inline-scripts --strip-comments _includes\polymer.html >_includes\polymer-vulcanized.temp.html
+
+echo {%%raw%%} >_includes\polymer-vulcanized.html
+type _includes\polymer-vulcanized.temp.html >>_includes\polymer-vulcanized.html
+echo {%%endraw%%} >>_includes\polymer-vulcanized.html
+del _includes\polymer-vulcanized.temp.html
