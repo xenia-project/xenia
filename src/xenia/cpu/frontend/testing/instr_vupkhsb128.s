52c2d03c3e0ef36a1df4b85d10494276f865aa6b
Dr. Chat <arkolbed@gmail.com>
2015-08-17 12:08:56 -0500

    Tests: vpkshss128/vpkshus128/vpkswss128/vpkswus128/vpkuhum128/vpkuhus128/vpkuwum128/vpkuwus128/vupkhsb128/vupklsb128

---
+++ src/xenia/cpu/frontend/testing/instr_vupkhsb128.s
@@ -0,0 +1,15 @@
+test_vupkhsb128_1:
+  # {-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7}
+  #_ REGISTER_IN v3 [F8F9FAFB, FCFDFEFF, 00010203, 04050607]
+  vupkhsb128 v3, v3
+  blr
+  # {-8, -7, -6, -5, -4, -3, -2, -1}
+  #_ REGISTER_OUT v3 [FFF8FFF9, FFFAFFFB, FFFCFFFD, FFFEFFFF]
+
+test_vupkhsb128_2:
+  # {0, 255, 255, 0, 0, 0, 255, 0, 255, 0, 0, 255, 255, 255, 0, 255}
+  #_ REGISTER_IN v3 [00FFFF00, 0000FF00, FF0000FF, FFFF00FF]
+  vupkhsb128 v3, v3
+  blr
+  # {0, 65535, 65535, 0, 0, 0, 65535, 0}
+  #_ REGISTER_OUT v3 [0000FFFF, FFFF0000, 00000000, FFFF0000]
---
+++ src/xenia/cpu/frontend/testing/instr_vupklsb128.s
@@ -0,0 +1,15 @@
+test_vupklsb128_1:
+  # {-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7}
+  #_ REGISTER_IN v3 [F8F9FAFB, FCFDFEFF, 00010203, 04050607]
+  vupklsb128 v3, v3
+  blr
+  # {0, 1, 2, 3, 4, 5, 6, 7}
+  #_ REGISTER_OUT v3 [00000001, 00020003, 00040005, 00060007]
+
+test_vupklsb128_2:
+  # {0, 255, 255, 0, 0, 0, 255, 0, 255, 0, 0, 255, 255, 255, 0, 255}
+  #_ REGISTER_IN v3 [00FFFF00, 0000FF00, FF0000FF, FFFF00FF]
+  vupklsb128 v3, v3
+  blr
+  # {65535, 0, 0, 65535, 65535, 65535, 0, 65535}
+  #_ REGISTER_OUT v3 [FFFF0000, 0000FFFF, FFFFFFFF, 0000FFFF]
