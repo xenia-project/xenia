2f9b37c142602ec7a244e37131f107350102b56b
Ben Vanik <ben.vanik@gmail.com>
2015-08-30 20:46:06 -0700

    Logging unimplemented instructions.

--- src/xenia/cpu/frontend/ppc_emit-private.h
+++ src/xenia/cpu/frontend/ppc_emit-private.h
@@ -10,6 +10,7 @@
 #ifndef XENIA_CPU_FRONTEND_PPC_EMIT_PRIVATE_H_
 #define XENIA_CPU_FRONTEND_PPC_EMIT_PRIVATE_H_
 
+#include "xenia/base/logging.h"
 #include "xenia/cpu/frontend/ppc_emit.h"
 #include "xenia/cpu/frontend/ppc_instr.h"
 
@@ -22,7 +23,9 @@ namespace frontend {
 #define XEREGISTERINSTR(name, opcode) \
   RegisterInstrEmit(opcode, (InstrEmitFn)InstrEmit_##name);
 
-#define XEINSTRNOTIMPLEMENTED() assert_always("Instruction not implemented");
+#define XEINSTRNOTIMPLEMENTED()                            \
+  XELOGE("Unimplemented instruction: %s", __FUNCTION__); \
+  assert_always("Instruction not implemented");
 
 }  // namespace frontend
 }  // namespace cpu
