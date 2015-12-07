9ed81b68769f98ae0b8684366966224b44c0c81f
Dr. Chat <arkolbed@gmail.com>
2015-12-07 10:53:45 -0600

    Fix not assigning write access to WriteCombine pages on restore.

--- src/xenia/memory.cc
+++ src/xenia/memory.cc
@@ -586,7 +586,8 @@ bool BaseHeap::Restore(ByteStream* stream) {
     }
 
     memory::PageAccess page_access = memory::PageAccess::kNoAccess;
-    if (page.current_protect == (kMemoryProtectRead | kMemoryProtectWrite)) {
+    if ((page.current_protect & kMemoryProtectRead) &&
+        (page.current_protect & kMemoryProtectWrite)) {
       page_access = memory::PageAccess::kReadWrite;
     } else if (page.current_protect & kMemoryProtectRead) {
       page_access = memory::PageAccess::kReadOnly;
