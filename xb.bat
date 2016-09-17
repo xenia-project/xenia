a6c703a0319df5c4a05a4f3ec7cdbfa12c67391c
Adam Baxter <github@voltagex.org>
2016-09-17 22:01:11 +1000

    Bump suggested Python 2.7 version in xb.bat

--- xb.bat
+++ xb.bat
@@ -11,7 +11,7 @@ CALL :check_python
 IF %_RESULT% NEQ 0 (
   ECHO.
   ECHO Python 2.7 must be installed and on PATH:
-  ECHO https://www.python.org/ftp/python/2.7.9/python-2.7.9.msi
+  ECHO https://www.python.org/ftp/python/2.7.12/python-2.7.12.msi
   GOTO :exit_error
 )
 
