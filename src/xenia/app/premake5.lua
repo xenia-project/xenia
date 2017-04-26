d0ac062701c06b6c28fd186f751d682dd22196a8
sephiroth99 <sephiroth99@gmail.com>
2017-04-26 02:13:09 -0400

    Add missing libraries when linking xenia-app on Linux
    Currently, each module in Xenia is built as a static library (also
    called archive). On Linux, an archive only contains the object files
    for that module. So any depedency that module has must be specified by
    adding those other modules when linking the program, as the dependencies
    are resolved at that time.

--- src/xenia/app/premake5.lua
+++ src/xenia/app/premake5.lua
@@ -8,8 +8,15 @@ project("xenia-app")
   targetname("xenia")
   language("C++")
   links({
+    "capstone",
     "gflags",
+    "glslang-spirv",
     "imgui",
+    "libavcodec",
+    "libavutil",
+    "snappy",
+    "spirv-tools",
+    "vulkan-loader",
     "xenia-apu",
     "xenia-apu-nop",
     "xenia-base",
@@ -20,10 +27,14 @@ project("xenia-app")
     "xenia-gpu",
     "xenia-gpu-null",
     "xenia-gpu-vulkan",
+    "xenia-hid",
     "xenia-hid-nop",
     "xenia-kernel",
     "xenia-ui",
+    "xenia-ui-spirv",
+    "xenia-ui-vulkan",
     "xenia-vfs",
+    "xxhash",
   })
   flags({
     "WinMain",  -- Use WinMain instead of main.
