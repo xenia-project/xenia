988877f42848e14855f7b678a2eaf7aaa507b716
Rick Gibbed <rick@gibbed.us>
2018-05-11 15:58:27 -0500

    [Vulkan] Use BC4 for DXT5A.

--- src/xenia/gpu/vulkan/texture_cache.cc
+++ src/xenia/gpu/vulkan/texture_cache.cc
@@ -115,7 +115,7 @@ static const TextureConfig texture_configs[64] = {
     /* k_11_11_10_AS_16_16_16_16 */ {VK_FORMAT_B10G11R11_UFLOAT_PACK32},  // ?
     /* k_32_32_32_FLOAT         */ {VK_FORMAT_R32G32B32_SFLOAT},
     /* k_DXT3A                  */ {VK_FORMAT_UNDEFINED},
-    /* k_DXT5A                  */ {VK_FORMAT_UNDEFINED},  // ATI1N
+    /* k_DXT5A                  */ {VK_FORMAT_BC4_UNORM_BLOCK},  // ATI1N
 
     // http://fileadmin.cs.lth.se/cs/Personal/Michael_Doggett/talks/unc-xenos-doggett.pdf
     /* k_CTX1                   */ {VK_FORMAT_R8G8_UINT},
