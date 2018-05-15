2dc145a0dc4b7670e540b1a2935396201bba8b02
Dr. Chat <arkolbed@gmail.com>
2018-05-15 14:04:18 -0500

    [Qt] Setup main window icon

--- src/xenia/app/xenia_main.cc
+++ src/xenia/app/xenia_main.cc
@@ -33,6 +33,7 @@ int xenia_main(const std::vector<std::wstring>& args) {
   char* argv[] = {"xenia", nullptr};
   QApplication app(argc, argv);
   EmulatorWindow main_wnd;
+  main_wnd.setWindowIcon(QIcon(":/icon.ico"));
 
   main_wnd.setFixedSize(1280, 720);
   main_wnd.show();
