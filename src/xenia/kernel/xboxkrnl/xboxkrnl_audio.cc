09c4bf604ee29c8fa7418b96ec4cec14fd5664bc
gibbed <rick@gibbed.us>
2018-05-24 23:31:21 -0500

    [Kernel] Add yield to XAudioGetVoiceCategoryVolumeChangeMask for games that spin threads with it.

--- src/xenia/kernel/xboxkrnl/xboxkrnl_audio.cc
+++ src/xenia/kernel/xboxkrnl/xboxkrnl_audio.cc
@@ -30,6 +30,8 @@ dword_result_t XAudioGetVoiceCategoryVolumeChangeMask(lpunknown_t driver_ptr,
                                                       lpdword_t out_ptr) {
   assert_true((driver_ptr.guest_address() & 0xFFFF0000) == 0x41550000);
 
+  xe::threading::MaybeYield();
+
   // Checking these bits to see if any voice volume changed.
   // I think.
   *out_ptr = 0;
