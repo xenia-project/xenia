4ec84cd9c309d035d1e0a5621078e98c1f102f33
Triang3l <triang3l@yandex.ru>
2018-07-18 11:42:51 +0300

    [D3D12] Create the project and link D3D libraries

---
+++ src/xenia/gpu/d3d12/premake5.lua
@@ -0,0 +1,21 @@
+project_root = "../../../.."
+include(project_root.."/tools/build")
+
+group("src")
+project("xenia-gpu-d3d12")
+  uuid("c057eae4-e7bb-4113-9a69-1fe07b735c49")
+  kind("StaticLib")
+  language("C++")
+  links({
+    "xenia-base",
+    "xenia-gpu",
+    "xxhash",
+  })
+  defines({
+  })
+  includedirs({
+    project_root.."/third_party/gflags/src",
+  })
+  local_platform_files()
+  files({
+  })
