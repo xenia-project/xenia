5500cd53746c0e7c01893708bcb5fe04e6d88fe0
gibbed <rick@gibbed.us>
2018-10-23 16:43:31 -0500

    [Kernel] Stub HidReadKeys.

---
+++ src/xenia/kernel/xboxkrnl/xboxkrnl_hid.cc
@@ -0,0 +1,40 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2013 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#include "xenia/base/logging.h"
+#include "xenia/kernel/kernel_state.h"
+#include "xenia/kernel/util/shim_utils.h"
+#include "xenia/kernel/xboxkrnl/xboxkrnl_private.h"
+#include "xenia/xbox.h"
+
+namespace xe {
+namespace kernel {
+namespace xboxkrnl {
+
+dword_result_t HidReadKeys(dword_t unk1, unknown_t unk2, unknown_t unk3) {
+  /* TODO(gibbed):
+   * Games check for the following errors:
+   *   0xC000009D - translated to 0x48F  - ERROR_DEVICE_NOT_CONNECTED
+   *   0x103      - translated to 0x10D2 - ERROR_EMPTY
+   * Other errors appear to be ignored?
+   *
+   * unk1 is 0
+   * unk2 is a pointer to &unk3[2], possibly a 6-byte vuffer
+   * unk3 is a pointer to a 20-byte buffer
+   */
+  return 0xC000009D;
+}
+DECLARE_XBOXKRNL_EXPORT(HidReadKeys, ExportTag::kStub);
+
+void RegisterHidExports(xe::cpu::ExportResolver* export_resolver,
+                        KernelState* kernel_state) {}
+
+}  // namespace xboxkrnl
+}  // namespace kernel
+}  // namespace xe
