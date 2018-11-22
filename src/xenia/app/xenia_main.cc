00261b9ca0389b27fc619735a2f6ae9c16b37ecb
gibbed <rick@gibbed.us>
2018-11-22 17:40:22 -0600

    [App] Get absolute path of content root in all cases.

--- src/xenia/app/xenia_main.cc
+++ src/xenia/app/xenia_main.cc
@@ -161,9 +161,8 @@ int xenia_main(const std::vector<std::wstring>& args) {
 #endif
       content_root = xe::join_paths(content_root, L"content");
     }
-
-    content_root = xe::to_absolute_path(content_root);
   }
+  content_root = xe::to_absolute_path(content_root);
 
   // Create the emulator but don't initialize so we can setup the window.
   auto emulator = std::make_unique<Emulator>(L"", content_root);
