74313585bf5fd3453ea4a3ce1b9ed2b567637fac
Sandy Carter <bwrsandman@gmail.com>
2019-01-20 21:28:17 -0500

    [string] Remove reuse of va_list in AppendVarargs
    va_list are not guarenteed to maintain their values after being used.
    With clang on Linux, args is undefined after fetching length and will print
    "(null)".
    Copy args into another va_list before getting length to prevent this.
    Add tests.

---
+++ src/xenia/base/testing/string_test.cc
@@ -0,0 +1,34 @@
+/**
+******************************************************************************
+* Xenia : Xbox 360 Emulator Research Project                                 *
+******************************************************************************
+* Copyright 2019 Ben Vanik. All rights reserved.                             *
+* Released under the BSD license - see LICENSE in the root for more details. *
+******************************************************************************
+*/
+
+#include "xenia/base/string.h"
+#include "xenia/base/string_buffer.h"
+
+#include "third_party/catch/include/catch.hpp"
+
+namespace xe {
+namespace base {
+namespace test {
+
+TEST_CASE("StringBuffer") {
+  StringBuffer sb;
+  uint32_t module_flags = 0x1000000;
+
+  std::string path_(R"(\Device\Cdrom0\default.xex)");
+  sb.AppendFormat("Module {}:\n", path_.c_str());
+  REQUIRE(sb.to_string() == "Module \\Device\\Cdrom0\\default.xex:\n");
+  sb.AppendFormat("    Module Flags: {:08X}\n", module_flags);
+  REQUIRE(
+      sb.to_string() ==
+      "Module \\Device\\Cdrom0\\default.xex:\n    Module Flags: 01000000\n");
+}
+
+}  // namespace test
+}  // namespace base
+}  // namespace xe
