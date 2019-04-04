782f4355489113c71c5734a95d46e0688e7615bd
Dr. Chat <arkolbed@gmail.com>
2019-04-04 18:20:08 -0500

    Add heavily WIP / nonworking networking code

---
+++ src/xenia/kernel/xsession.h
@@ -0,0 +1,31 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2015 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#ifndef XENIA_KERNEL_XSESSION_H_
+#define XENIA_KERNEL_XSESSION_H_
+
+#include "xenia/base/byte_order.h"
+#include "xenia/kernel/xobject.h"
+
+namespace xe {
+namespace kernel {
+class XSession : public XObject {
+ public:
+  static const Type kType = kTypeSession;
+
+  XSession(KernelState* kernel_state);
+
+  bool Initialize();
+
+ private:
+};
+}  // namespace kernel
+}  // namespace xe
+
+#endif  // XENIA_KERNEL_XSESSION_H_
