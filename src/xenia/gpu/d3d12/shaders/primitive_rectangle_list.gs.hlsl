42cf553e7fcdeda4a3f51b57b5229dc375067aa6
Triang3l <triang3l@yandex.ru>
2019-04-09 17:19:47 +0300

    [D3D12] Draw rectangles by mirroring one vertex across diagonal

--- src/xenia/gpu/d3d12/shaders/primitive_rectangle_list.gs.hlsl
+++ src/xenia/gpu/d3d12/shaders/primitive_rectangle_list.gs.hlsl
@@ -7,15 +7,17 @@ void main(triangle XeVertex xe_in[3],
   xe_stream.Append(xe_in[1]);
   xe_stream.Append(xe_in[2]);
   xe_stream.RestartStrip();
-
-  XeVertex xe_out;
-  xe_out.point_params.z = xe_in[2].point_params.z;
-  xe_out.clip_space_zw = xe_in[2].clip_space_zw;
-  // Most games use a left-aligned form.
-  [branch] if (all(xe_in[0].position.xy ==
-                   float2(xe_in[2].position.x, xe_in[1].position.y)) ||
-               all(xe_in[0].position.xy ==
-                   float2(xe_in[1].position.x, xe_in[2].position.y))) {
+  // Find the diagonal (the edge that is longer than both the other two) and
+  // mirror the other vertex across it.
+  float3 edge_01 = xe_in[1].position.xyz - xe_in[0].position.xyz;
+  float3 edge_02 = xe_in[2].position.xyz - xe_in[0].position.xyz;
+  float3 edge_12 = xe_in[2].position.xyz - xe_in[1].position.xyz;
+  float3 edge_squares = float3(
+      dot(edge_01, edge_01), dot(edge_02, edge_02), dot(edge_12, edge_12));
+  float3 v3_signs;
+  if (edge_squares.z > edge_squares.x && edge_squares.z > edge_squares.y) {
+    // 12 is the diagonal. Most games use this form.
+    //
     //  0 ------ 1   0:  -1,-1
     //  |      - |   1:   1,-1
     //  |   //   |   2:  -1, 1
@@ -29,19 +31,11 @@ void main(triangle XeVertex xe_in[3],
     //  1 ------[3]
     xe_stream.Append(xe_in[2]);
     xe_stream.Append(xe_in[1]);
-    [unroll] for (int i = 0; i < 16; ++i) {
-      xe_out.interpolators[i] = xe_in[1].interpolators[i] -
-                                xe_in[0].interpolators[i] +
-                                xe_in[2].interpolators[i];
-    }
-    xe_out.point_params.xy = xe_in[1].point_params.xy +
-                             xe_in[0].point_params.xy -
-                             xe_in[2].point_params.xy;
-    xe_out.position = float4(xe_in[1].position.xy -
-                             xe_in[0].position.xy +
-                             xe_in[2].position.xy,
-                             xe_in[2].position.zw);
-  } else {
+    v3_signs = float3(-1.0f, 1.0f, 1.0f);
+  } else if (edge_squares.y > edge_squares.x &&
+             edge_squares.y > edge_squares.z) {
+    // 02 is the diagonal.
+    //
     //  0 ------ 1   0:  -1,-1
     //  | -      |   1:   1,-1
     //  |   \\   |   2:   1, 1
@@ -49,19 +43,34 @@ void main(triangle XeVertex xe_in[3],
     // [3] ----- 2
     xe_stream.Append(xe_in[0]);
     xe_stream.Append(xe_in[2]);
-    [unroll] for (int i = 0; i < 16; ++i) {
-      xe_out.interpolators[i] = xe_in[0].interpolators[i] -
-                                xe_in[1].interpolators[i] +
-                                xe_in[2].interpolators[i];
-    }
-    xe_out.point_params.xy = xe_in[0].point_params.xy +
-                             xe_in[1].point_params.xy -
-                             xe_in[2].point_params.xy;
-    xe_out.position = float4(xe_in[0].position.xy -
-                             xe_in[1].position.xy +
-                             xe_in[2].position.xy,
-                             xe_in[2].position.zw);
+    v3_signs = float3(1.0f, -1.0f, 1.0f);
+  } else {
+    // 01 is the diagonal. Not seen in any game so far.
+    //
+    //  0 ------ 2   0:  -1,-1
+    //  | -      |   1:   1, 1
+    //  |   \\   |   2:   1,-1
+    //  |      - |   3: [-1, 1 ]
+    // [3] ----- 1
+    xe_stream.Append(xe_in[1]);
+    xe_stream.Append(xe_in[0]);
+    v3_signs = float3(1.0f, 1.0f, -1.0f);
   }
+  XeVertex xe_out;
+  [unroll] for (int i = 0; i < 16; ++i) {
+    xe_out.interpolators[i] = v3_signs.x * xe_in[0].interpolators[i] +
+                              v3_signs.y * xe_in[1].interpolators[i] +
+                              v3_signs.z * xe_in[2].interpolators[i];
+  }
+  xe_out.point_params = v3_signs.x * xe_in[0].point_params +
+                        v3_signs.y * xe_in[1].point_params +
+                        v3_signs.z * xe_in[2].point_params;
+  xe_out.clip_space_zw = v3_signs.x * xe_in[0].clip_space_zw +
+                         v3_signs.y * xe_in[1].clip_space_zw +
+                         v3_signs.z * xe_in[2].clip_space_zw;
+  xe_out.position = v3_signs.x * xe_in[0].position +
+                    v3_signs.y * xe_in[1].position +
+                    v3_signs.z * xe_in[2].position;
   xe_stream.Append(xe_out);
   xe_stream.RestartStrip();
 }
