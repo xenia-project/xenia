aeb6d2aa506039cda0aeecfcf7413e7a3a355582
ObsidianNA <42883452+ObsidianNA@users.noreply.github.com>
2019-07-09 14:43:38 -0400

    List of recently opened files
    Overview:
    Added a Pop-up menu named 'Open Recent' under the 'File' menu bar item that lists 10 recently opened files.
    
    It stores the paths of opened games inside a file named 'recent.txt' in the same folder where the xenia.exe is located.
    
    If the file doesn't exits, the program creates it, then paths are stored when a new file is opened.
    
    Each time a file is opened, the program compares stored paths with the new one, if it exists then the path is moved to the top of the list to indicate that it is the most recent file opened.

---
+++ emulator_window.h
@@ -0,0 +1,83 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2015 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#ifndef XENIA_APP_EMULATOR_WINDOW_H_
+#define XENIA_APP_EMULATOR_WINDOW_H_
+
+#include <memory>
+#include <string>
+
+#include "xenia/ui/loop.h"
+#include "xenia/ui/menu_item.h"
+#include "xenia/ui/window.h"
+#include "xenia/xbox.h"
+
+namespace xe {
+class Emulator;
+}  // namespace xe
+
+namespace xe {
+namespace app {
+
+class EmulatorWindow {
+ public:
+  virtual ~EmulatorWindow();
+
+  static std::unique_ptr<EmulatorWindow> Create(Emulator* emulator);
+
+  Emulator* emulator() const { return emulator_; }
+  ui::Loop* loop() const { return loop_.get(); }
+  ui::Window* window() const { return window_.get(); }
+
+  void UpdateTitle();
+  void ToggleFullscreen();
+
+ private:
+  explicit EmulatorWindow(Emulator* emulator);
+
+  bool Initialize();
+
+  void FileDrop(wchar_t* filename);
+  void FileOpen();
+  void RecentListUpdater(std::wstring PATH);
+  void RecentList(int index);
+  void RecentList0();
+  void RecentList1();
+  void RecentList2();
+  void RecentList3();
+  void RecentList4();
+  void RecentList5();
+  void RecentList6();
+  void RecentList7();
+  void RecentList8();
+  void RecentList9();
+  void FileClose();
+  void ShowContentDirectory();
+  void CheckHideCursor();
+  void CpuTimeScalarReset();
+  void CpuTimeScalarSetHalf();
+  void CpuTimeScalarSetDouble();
+  void CpuBreakIntoDebugger();
+  void CpuBreakIntoHostDebugger();
+  void GpuTraceFrame();
+  void GpuClearCaches();
+  void ShowHelpWebsite();
+
+  Emulator* emulator_;
+  std::unique_ptr<ui::Loop> loop_;
+  std::unique_ptr<ui::Window> window_;
+  std::wstring base_title_;
+  std::wstring GLOBAL_RECENT_PATHS[10];
+  uint64_t cursor_hide_time_ = 0;
+};
+
+}  // namespace app
+}  // namespace xe
+
+#endif  // XENIA_APP_EMULATOR_WINDOW_H_
