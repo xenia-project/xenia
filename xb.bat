9cfbe06bef8623d839597c12db89e9e0dadc6ed5
chris <chridye@gmail.com>
2019-07-19 23:55:17 -0400

    properly detect the fake python 3 in the latest windows 10 build

--- xb.bat
+++ xb.bat
@@ -1,4 +1,4 @@
-@ECHO OFF
+@echo off
 REM Copyright 2015 Ben Vanik. All Rights Reserved.
 
 SET DIR=%~dp0
@@ -29,12 +29,10 @@ REM Utilities
 REM ============================================================================
 
 :check_python
-SETLOCAL
+setlocal enabledelayedexpansion
 SET FOUND_PYTHON_EXE=""
-1>NUL 2>NUL CMD /c where python3
-IF NOT ERRORLEVEL 1 (
-  SET FOUND_PYTHON_EXE=python3
-)
+
+
 IF %FOUND_PYTHON_EXE% EQU "" (
   IF EXIST c:\\python34\\python.exe SET FOUND_PYTHON_EXE=C:\\python34\\python.exe
 )
@@ -45,24 +43,45 @@ IF %FOUND_PYTHON_EXE% EQU "" (
   IF EXIST c:\\python36\\python.exe SET FOUND_PYTHON_EXE=C:\\python36\\python.exe
 )
 IF %FOUND_PYTHON_EXE% EQU "" (
-  1>NUL 2>NUL CMD /c where python
-  IF NOT ERRORLEVEL 1 (
-    SET FOUND_PYTHON_EXE=python
-  )
+  IF EXIST c:\\python37\\python.exe SET FOUND_PYTHON_EXE=C:\\python37\\python.exe
 )
+IF %FOUND_PYTHON_EXE% EQU "" (
+  IF EXIST c:\\python37\\python.exe SET FOUND_PYTHON_EXE=C:\\python37\\python.exe
+)
+
 IF %FOUND_PYTHON_EXE% EQU "" (
   ECHO ERROR: no Python executable found on PATH.
   ECHO Make sure you can run 'python' or 'python3' in a Command Prompt.
-  ENDLOCAL & SET _RESULT=1
+  ENDLOCAL enabledelayedexpansion & SET _RESULT=1
   GOTO :eof
 )
 CMD /C %FOUND_PYTHON_EXE% -c "import sys; sys.exit(1 if not sys.version_info[:2] >= (3, 4) else 0)"
 IF %ERRORLEVEL% NEQ 0 (
   ECHO ERROR: Python version mismatch - not 3.4+
-  ENDLOCAL & SET _RESULT=1
+  ENDLOCAL enabledelayedexpansion & SET _RESULT=1
   GOTO :eof
 )
-ENDLOCAL & (
+
+FOR /F "delims=" %%I IN ('where python') DO (
+	CMD /C %%I -c "import sys; sys.exit(1 if not sys.version_info[:2] >= (3, 4) else 0)"
+	ENDLOCAL enabledelayedexpansion
+	IF %ERRORLEVEL% EQU 0 (
+	  SET _RESULT=0
+	  SET PYTHON_EXE=%%I
+	  GOTO :eof
+	)
+)
+FOR /F "delims=" %%I IN ('where python3') DO (
+	CMD /C %%I -c "import sys; sys.exit(1 if not sys.version_info[:2] >= (3, 4) else 0)"
+	ENDLOCAL enabledelayedexpansion
+	IF %ERRORLEVEL% EQU 0 (
+	  SET _RESULT=0
+	  SET PYTHON_EXE=%%I
+	  GOTO :eof
+	)
+)
+
+ENDLOCAL enabledelayedexpansion & (
   SET _RESULT=0
   SET PYTHON_EXE=%FOUND_PYTHON_EXE%
 )
