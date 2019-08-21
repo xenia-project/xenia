ca96ae9480e192da7a41c1bd155ceca07cb7cb5a
Sandy Carter <bwrsandman@gmail.com>
2019-08-21 23:41:35 +0200

    [linux graphics] Remove GLEW
    OpenGL is not used in the whole stack.
    All references are removed.

--- src/xenia/gpu/vulkan/premake5.lua
+++ src/xenia/gpu/vulkan/premake5.lua
@@ -70,7 +70,6 @@ project("xenia-gpu-vulkan-trace-viewer")
       "X11",
       "xcb",
       "X11-xcb",
-      "GL",
     })
 
   filter("platforms:Windows")
@@ -137,7 +136,6 @@ project("xenia-gpu-vulkan-trace-dump")
       "X11",
       "xcb",
       "X11-xcb",
-      "GL",
     })
 
   filter("platforms:Windows")
