96c0bf9ccd597fa3642c2b41196489dcac59c8a7
Triang3l <triang3l@yandex.ru>
2019-10-27 00:36:55 +0300

    [D3D12] Trace viewer entry point

--- src/xenia/gpu/d3d12/premake5.lua
+++ src/xenia/gpu/d3d12/premake5.lua
@@ -19,6 +19,50 @@ project("xenia-gpu-d3d12")
   })
 
 group("src")
+project("xenia-gpu-d3d12-trace-viewer")
+  uuid("7b5b9fcb-7bf1-43ff-a774-d4c41c8706be")
+  kind("WindowedApp")
+  language("C++")
+  links({
+    "aes_128",
+    "capstone",
+    "dxbc",
+    "imgui",
+    "libavcodec",
+    "libavutil",
+    "mspack",
+    "snappy",
+    "xenia-apu",
+    "xenia-apu-nop",
+    "xenia-base",
+    "xenia-core",
+    "xenia-cpu",
+    "xenia-cpu-backend-x64",
+    "xenia-gpu",
+    "xenia-gpu-d3d12",
+    "xenia-hid",
+    "xenia-hid-nop",
+    "xenia-kernel",
+    "xenia-ui",
+    "xenia-ui-d3d12",
+    "xenia-vfs",
+    "xxhash",
+  })
+  files({
+    "d3d12_trace_viewer_main.cc",
+    "../../base/main_"..platform_suffix..".cc",
+  })
+  -- Only create the .user file if it doesn't already exist.
+  local user_file = project_root.."/build/xenia-gpu-d3d12-trace-viewer.vcxproj.user"
+  if not os.isfile(user_file) then
+    debugdir(project_root)
+    debugargs({
+      "2>&1",
+      "1>scratch/stdout-trace-viewer.txt",
+    })
+  end
+
+group("src")
 project("xenia-gpu-d3d12-trace-dump")
   uuid("686b859c-0046-44c4-a02c-41fc3fb75698")
   kind("ConsoleApp")
