1ea6e5f1abb84879577aaca081275cf422f5bd06
gibbed <rick@gibbed.us>
2020-02-21 09:44:45 -0600

    Add fmt submodule.

---
+++ third_party/fmt.lua
@@ -0,0 +1,17 @@
+group("third_party")
+project("fmt")
+  uuid("b9ff4b2c-b438-42a9-971e-e0c19a711a13")
+  kind("StaticLib")
+  language("C++")
+  links({
+  })
+  defines({
+    "_LIB",
+  })
+  includedirs({
+    "fmt/include",
+  })
+  files({
+    "fmt/src/format.cc",
+    "fmt/src/os.cc"
+  })
