c62fbe7936be63867c57590f591b9760ca7f9108
gibbed <rick@gibbed.us>
2020-04-06 16:50:55 -0500

    [PPC] Disable frsqrte tests.

--- src/xenia/cpu/ppc/testing/instr_frsqrte.s
+++ src/xenia/cpu/ppc/testing/instr_frsqrte.s
@@ -1,19 +1,21 @@
-test_frsqrte_1:
+# frsqrte tests disabled because accuracy is CPU dependent.
+
+#test_frsqrte_1:
   #_ REGISTER_IN f1 1.0
-  frsqrte f1, f1
-  blr
+#  frsqrte f1, f1
+#  blr
   #_ REGISTER_OUT f1 0.99975585937500000
   # want: 0.97
 
-test_frsqrte_2:
+#test_frsqrte_2:
   #_ REGISTER_IN f1 64.0
-  frsqrte f1, f1
-  blr
+#  frsqrte f1, f1
+#  blr
   #_ REGISTER_OUT f1 0.12496948242187500
 
-test_frsqrte_3:
+#test_frsqrte_3:
   #_ REGISTER_IN f1 0.5
-  frsqrte f1, f1
-  blr
+#  frsqrte f1, f1
+#  blr
   #_ REGISTER_OUT f1 1.41381835937500000
   # want: 1.375
