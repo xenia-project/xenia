cc367af94a90a592e2b8bae36b4f8d13850184cf
gibbed <rick@gibbed.us>
2020-04-09 16:48:05 -0500

    [Kernel] Stub a few more XBDM functions.

--- src/xenia/kernel/xbdm/xbdm_misc.cc
+++ src/xenia/kernel/xbdm/xbdm_misc.cc
@@ -2,7 +2,7 @@
  ******************************************************************************
  * Xenia : Xbox 360 Emulator Research Project                                 *
  ******************************************************************************
- * Copyright 2013 Ben Vanik. All rights reserved.                             *
+ * Copyright 2020 Ben Vanik. All rights reserved.                             *
  * Released under the BSD license - see LICENSE in the root for more details. *
  ******************************************************************************
  */
@@ -18,21 +18,48 @@ namespace xe {
 namespace kernel {
 namespace xbdm {
 
+#define MAKE_DUMMY_STUB_PTR(x)     \
+  dword_result_t x() { return 0; } \
+  DECLARE_XBDM_EXPORT1(x, kDebug, kStub)
+
+#define MAKE_DUMMY_STUB_STATUS(x)                           \
+  dword_result_t x() { return X_STATUS_INVALID_PARAMETER; } \
+  DECLARE_XBDM_EXPORT1(x, kDebug, kStub)
+
+MAKE_DUMMY_STUB_PTR(DmAllocatePool);
+
 void DmCloseLoadedModules(lpdword_t unk0_ptr) {}
 DECLARE_XBDM_EXPORT1(DmCloseLoadedModules, kDebug, kStub);
+
+MAKE_DUMMY_STUB_STATUS(DmFreePool);
+
+MAKE_DUMMY_STUB_STATUS(DmGetXbeInfo);
+MAKE_DUMMY_STUB_STATUS(DmGetXboxName);
+
+dword_result_t DmIsDebuggerPresent() { return 0; }
+DECLARE_XBDM_EXPORT1(DmIsDebuggerPresent, kDebug, kStub);
+
+MAKE_DUMMY_STUB_STATUS(DmRegisterCommandProcessor);
 
 void DmSendNotificationString(lpdword_t unk0_ptr) {}
 DECLARE_XBDM_EXPORT1(DmSendNotificationString, kDebug, kStub);
 
-dword_result_t DmWalkLoadedModules(lpdword_t unk0_ptr, lpdword_t unk1_ptr) {
-  return X_STATUS_INVALID_PARAMETER;
-}
-DECLARE_XBDM_EXPORT1(DmWalkLoadedModules, kDebug, kStub);
+MAKE_DUMMY_STUB_STATUS(DmRegisterCommandProcessorEx);
+MAKE_DUMMY_STUB_STATUS(DmStartProfiling);
+MAKE_DUMMY_STUB_STATUS(DmStopProfiling);
 
 dword_result_t DmCaptureStackBackTrace(lpdword_t unk0_ptr, lpdword_t unk1_ptr) {
   return X_STATUS_INVALID_PARAMETER;
 }
 DECLARE_XBDM_EXPORT1(DmCaptureStackBackTrace, kDebug, kStub);
+
+MAKE_DUMMY_STUB_STATUS(DmGetThreadInfoEx);
+MAKE_DUMMY_STUB_STATUS(DmSetProfilingOptions);
+
+dword_result_t DmWalkLoadedModules(lpdword_t unk0_ptr, lpdword_t unk1_ptr) {
+  return X_STATUS_INVALID_PARAMETER;
+}
+DECLARE_XBDM_EXPORT1(DmWalkLoadedModules, kDebug, kStub);
 
 void DmMapDevkitDrive() {}
 DECLARE_XBDM_EXPORT1(DmMapDevkitDrive, kDebug, kStub);
