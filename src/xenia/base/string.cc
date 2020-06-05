f76449b927446bd92866c79cb551c51a87485c84
Gliniak <Gliniak93@gmail.com>
2020-06-05 22:46:46 +0200

    [Base] Wrap to_utf functions with try catch
    This will prevent some games from crashing due to poor naming (garbage naming)

--- src/xenia/base/string.cc
+++ src/xenia/base/string.cc
@@ -8,6 +8,7 @@
  */
 
 #include "xenia/base/string.h"
+#include "xenia/base/logging.h"
 
 #include <algorithm>
 #include <locale>
@@ -20,11 +21,16 @@ namespace utfcpp = utf8;
 namespace xe {
 
 std::string to_utf8(const std::u16string_view source) {
-  return utfcpp::utf16to8(source);
+  try {
+    return utfcpp::utf16to8(source);
+  } catch (const utfcpp::invalid_utf16& e) {
+    XELOGW("Invalid to_utf8 conversion: {} - {:04X}", e.what(), e.utf16_word());
+    return std::string();
+  }
 }
 
 std::u16string to_utf16(const std::string_view source) {
-  return utfcpp::utf8to16(source);
+  return utfcpp::utf8to16(utfcpp::replace_invalid(source));
 }
 
 }  // namespace xe
