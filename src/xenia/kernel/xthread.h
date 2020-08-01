57e402106863a00d270610f8ebb7b74a17ef7f20
Gliniak <Gliniak93@gmail.com>
2020-08-01 08:13:54 +0200

    [Kernel/Thread] Changed default stack location

--- src/xenia/kernel/xthread.h
+++ src/xenia/kernel/xthread.h
@@ -149,6 +149,9 @@ class XThread : public XObject, public cpu::Thread {
  public:
   static const XObject::Type kObjectType = XObject::Type::Thread;
 
+  static constexpr uint32_t kStackAddressRangeBegin = 0x70000000;
+  static constexpr uint32_t kStackAddressRangeEnd = 0x7F000000;
+
   struct CreationParams {
     uint32_t stack_size;
     uint32_t xapi_thread_startup;
