38b81f1c7e840211cba80565baff6eeef1b8e4ad
Triang3l <triang3l@yandex.ru>
2020-11-21 17:14:40 +0300

    [Build] Make premake-androidmk generate something

--- third_party/cpptoml.lua
+++ third_party/cpptoml.lua
@@ -1,7 +1,12 @@
 group("third_party")
 project("cpptoml")
   uuid("1e86cc51-3f8b-476d-9249-3b200424846b")
-  kind("Utility")
+  if os.istarget("android") then
+    -- ndk-build only supports StaticLib and SharedLib.
+    kind("StaticLib")
+  else
+    kind("Utility")
+  end
   language("C++")
   files({
     "cpptoml/include/cpptoml.h",
