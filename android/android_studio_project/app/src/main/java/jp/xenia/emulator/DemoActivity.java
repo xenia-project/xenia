73dad88429a83c1d007cb672e288d2becbd2c195
Triang3l <triang3l@yandex.ru>
2020-11-21 22:00:48 +0300

    [Android] Studio project enough to invoke NDK-Build

---
+++ android/android_studio_project/app/src/main/java/jp/xenia/emulator/DemoActivity.java
@@ -0,0 +1,12 @@
+package jp.xenia.emulator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DemoActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
