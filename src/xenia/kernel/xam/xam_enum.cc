c3f0fbe8f6f72dc47fc6a78c5300c4fdb6bd35a6
Gliniak <Gliniak93@gmail.com>
2021-05-06 16:32:12 +0200

    [Kernel/XAM] Fixed logic in XamEnumerate checks order

--- src/xenia/kernel/xam/xam_enum.cc
+++ src/xenia/kernel/xam/xam_enum.cc
@@ -54,10 +54,10 @@ uint32_t xeXamEnumerate(uint32_t handle, uint32_t flags, void* buffer,
 
     std::memset(buffer, 0, actual_buffer_length);
 
-    if (actual_buffer_length < e->item_size()) {
-      result = X_ERROR_INSUFFICIENT_BUFFER;
-    } else if (e->current_item() >= e->item_count()) {
+    if (e->current_item() >= e->item_count()) {
       result = X_ERROR_NO_MORE_FILES;
+    } else if (actual_buffer_length < e->item_size()) {
+      result = X_ERROR_INSUFFICIENT_BUFFER;
     } else {
       auto item_buffer = static_cast<uint8_t*>(buffer);
       auto max_items = actual_buffer_length / e->item_size();
