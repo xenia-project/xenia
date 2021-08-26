f540c188bf16abf3b3be4ffd00d1154fb12e5254
Triang3l <triang3l@yandex.ru>
2021-08-26 21:18:18 +0300

    [Lint] Revert incorrect clang-format changes

--- src/xenia/cpu/hir/value.cc
+++ src/xenia/cpu/hir/value.cc
@@ -395,8 +395,9 @@ void Value::MulHi(Value* other, bool is_unsigned) {
                                   (uint32_t)other->constant.i32) >>
                                  32);
       } else {
-        constant.i32 = (int32_t)(
-            ((int64_t)constant.i32 * (int64_t)other->constant.i32) >> 32);
+        constant.i32 =
+            (int32_t)(((int64_t)constant.i32 * (int64_t)other->constant.i32) >>
+                      32);
       }
       break;
     case INT64_TYPE:
--- src/xenia/cpu/ppc/ppc_emit_control.cc
+++ src/xenia/cpu/ppc/ppc_emit_control.cc
@@ -810,6 +810,6 @@ void RegisterEmitCategoryControl() {
   XEREGISTERINSTR(mtmsrd);
 }
 
-}  // namespace cpu
+}  // namespace ppc
 }  // namespace cpu
 }  // namespace xe
