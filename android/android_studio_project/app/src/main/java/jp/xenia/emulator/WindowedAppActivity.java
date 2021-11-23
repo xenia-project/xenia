2b302676063c73beb9d3b98be7cb11f434de0528
Sahil <43530216+sahil-bhandari@users.noreply.github.com>
2021-11-23 19:22:57 +0530

    Add Runtime Permissions
    With the introduction of Android 6.0 (SDK 23), users are prompted for some specific permissions at runtime when they become necessary to use.

--- android/android_studio_project/app/src/main/java/jp/xenia/emulator/WindowedAppActivity.java
+++ android/android_studio_project/app/src/main/java/jp/xenia/emulator/WindowedAppActivity.java
@@ -1,12 +1,17 @@
 package jp.xenia.emulator;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public abstract class WindowedAppActivity extends Activity {
     private static final String TAG = "WindowedAppActivity";
+    private final int STORAGE_PERMISSION_CODE = 1;
 
     static {
         // TODO(Triang3l): Move all demos to libxenia.so.
@@ -30,7 +35,37 @@ public abstract class WindowedAppActivity extends Activity {
         if (mAppContext == 0) {
             Log.e(TAG, "Error initializing the windowed app");
             finish();
-            return;
+        }
+
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            //do if permission is granted
+        } else {
+            //request permission
+            requestStoragePermission();
+        }
+    }
+
+    private void requestStoragePermission() {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed because of this and that")
+                    .setPositiveButton(android.R.string.ok, (dialog, which) -> requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE))
+                    .setNegativeButton(android.R.string.cancel, (dialog, which) -> dialog.dismiss())
+                    .create().show();
+        } else {
+            requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE)  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
