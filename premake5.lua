0f40eeed82818b4a72d77f420b6d4f114ecf5ec2
Triang3l <triang3l@yandex.ru>
2022-05-05 21:46:20 +0300

    [UI] Windows: Disable rounded corners

--- premake5.lua
+++ premake5.lua
@@ -225,8 +225,9 @@ workspace("xenia")
       platforms({"Windows"})
       -- 10.0.15063.0: ID3D12GraphicsCommandList1::SetSamplePositions.
       -- 10.0.19041.0: D3D12_HEAP_FLAG_CREATE_NOT_ZEROED.
+      -- 10.0.22000.0: DWMWA_WINDOW_CORNER_PREFERENCE.
       filter("action:vs2017")
-        systemversion("10.0.19041.0")
+        systemversion("10.0.22000.0")
       filter("action:vs2019")
         systemversion("10.0")
       filter({})
