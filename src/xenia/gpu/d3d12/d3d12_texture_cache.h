2606fa57090085a0ebe00baca4e4045d7e7646bd
Triang3l <triang3l@yandex.ru>
2022-06-26 18:36:26 +0300

    [GPU] Apply BaseMap MipFilter via samplers as it may be overridden
    Make it have no effect on the texture resource as a resource may be used with samplers with different overrides. Also make sure magnification vs. minification is not undefined with it on Direct3D 12.

--- src/xenia/gpu/d3d12/d3d12_texture_cache.h
+++ src/xenia/gpu/d3d12/d3d12_texture_cache.h
@@ -58,7 +58,9 @@ class D3D12TextureCache final : public TextureCache {
       uint32_t mip_linear : 1;              // 14
       xenos::AnisoFilter aniso_filter : 3;  // 17
       uint32_t mip_min_level : 4;           // 21
-      // Maximum mip level is in the texture resource itself.
+      uint32_t mip_base_map : 1;            // 22
+      // Maximum mip level is in the texture resource itself, but mip_base_map
+      // can be used to limit fetching to mip_min_level.
     };
 
     SamplerParameters() : value(0) { static_assert_size(*this, sizeof(value)); }
