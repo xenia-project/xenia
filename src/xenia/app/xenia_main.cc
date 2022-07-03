ed61e15fc3636927a7d79e0098d21f9f64cae610
Triang3l <triang3l@yandex.ru>
2022-07-03 19:49:11 +0300

    [App] Make D3D12 the default GPU backend on Windows again

--- src/xenia/app/xenia_main.cc
+++ src/xenia/app/xenia_main.cc
@@ -260,11 +260,10 @@ std::unique_ptr<apu::AudioSystem> EmulatorApp::CreateAudioSystem(
 
 std::unique_ptr<gpu::GraphicsSystem> EmulatorApp::CreateGraphicsSystem() {
   Factory<gpu::GraphicsSystem> factory;
-  factory.Add<gpu::vulkan::VulkanGraphicsSystem>("vulkan");
-  // TODO(Triang3l): Move D3D12 back to the top.
 #if XE_PLATFORM_WIN32
   factory.Add<gpu::d3d12::D3D12GraphicsSystem>("d3d12");
 #endif  // XE_PLATFORM_WIN32
+  factory.Add<gpu::vulkan::VulkanGraphicsSystem>("vulkan");
   factory.Add<gpu::null::NullGraphicsSystem>("null");
   return factory.Create(cvars::gpu);
 }
