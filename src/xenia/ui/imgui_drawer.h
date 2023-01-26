540eff1ebf667308c97197db95bc6b36e6a2fca3
Roy Stewart <roythomasstewart@gmail.com>
2023-01-26 05:43:40 -0500

    Fix include order

--- src/xenia/ui/imgui_drawer.h
+++ src/xenia/ui/imgui_drawer.h
@@ -13,8 +13,8 @@
 #include <cstddef>
 #include <cstdint>
 #include <memory>
-#include <vector>
 #include <optional>
+#include <vector>
 
 #include "xenia/ui/immediate_drawer.h"
 #include "xenia/ui/presenter.h"
