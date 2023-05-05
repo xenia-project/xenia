ed64e3072bde74955c79b9f3b0030c133648d702
Triang3l <triang3l@yandex.ru>
2023-05-05 21:38:45 +0300

    [GPU] Remove implicit bool cast in memexport checks

--- src/xenia/gpu/vulkan/vulkan_command_processor.cc
+++ src/xenia/gpu/vulkan/vulkan_command_processor.cc
@@ -2175,7 +2175,7 @@ bool VulkanCommandProcessor::IssueDraw(xenos::PrimitiveType prim_type,
     return false;
   }
   pipeline_cache_->AnalyzeShaderUcode(*vertex_shader);
-  bool memexport_used_vertex = vertex_shader->memexport_eM_written();
+  bool memexport_used_vertex = vertex_shader->memexport_eM_written() != 0;
 
   // Pixel shader analysis.
   bool primitive_polygonal = draw_util::IsPrimitivePolygonal(regs);
