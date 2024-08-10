4c6e0ff6851c100bdecc2517c4779bc63df3c250
Gliniak <Gliniak93@gmail.com>
2024-08-10 16:29:46 +0200

    [Kernel] Added returning allocated size from MmAllocatePhysicalMemoryEx

--- src/xenia/kernel/xboxkrnl/xboxkrnl_memory.cc
+++ src/xenia/kernel/xboxkrnl/xboxkrnl_memory.cc
@@ -452,7 +452,8 @@ uint32_t xeMmAllocatePhysicalMemoryEx(uint32_t flags, uint32_t region_size,
     // Failed - assume no memory available.
     return 0;
   }
-  XELOGD("MmAllocatePhysicalMemoryEx = {:08X}", base_address);
+  XELOGD("MmAllocatePhysicalMemoryEx = {:08X} Size: {:08X}", base_address,
+         adjusted_size);
 
   return base_address;
 }
