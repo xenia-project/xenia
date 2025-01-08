7e51efeec5ff2bbce1ecae0df0aa5a85b0246833
Gliniak <Gliniak93@gmail.com>
2025-01-08 19:47:29 +0100

    [3PP] ZLib: Ignore warnings-errors

--- third_party/zlib.lua
+++ third_party/zlib.lua
@@ -3,6 +3,13 @@ project("zlib")
   uuid("13d4073d-f1c9-47e3-a057-79b133596fc2")
   kind("StaticLib")
   language("C")
+  cdialect("C90")
+  filter({"toolset:gcc or clang"})
+    buildoptions({
+      "-Wno-implicit-function-declaration"
+    })
+  filter {}
+
   files({
     "zlib/*.c",
     "zlib/*.h",
