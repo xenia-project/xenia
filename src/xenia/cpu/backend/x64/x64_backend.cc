9a0ed48168c074d6c512dfab6ed59260a00905eb
nikolay-kyosev <nikolay.kyosev@outlook.com>
2025-01-27 13:31:20 +0100

    A fix for the release build crash on linux.

--- src/xenia/cpu/backend/x64/x64_backend.cc
+++ src/xenia/cpu/backend/x64/x64_backend.cc
@@ -685,9 +685,6 @@ HostToGuestThunk X64HelperEmitter::EmitHostToGuestThunk() {
 
   code_offsets.prolog = getSize();
   // rsp + 0 = return address
-  mov(qword[rsp + 8 * 3], rdx);
-  mov(qword[rsp + 8 * 2], rsi);
-  mov(qword[rsp + 8 * 1], rdi);
   sub(rsp, stack_size);
 
   code_offsets.prolog_stack_alloc = getSize();
@@ -707,9 +704,6 @@ HostToGuestThunk X64HelperEmitter::EmitHostToGuestThunk() {
   code_offsets.epilog = getSize();
 
   add(rsp, stack_size);
-  mov(rdi, qword[rsp + 8 * 1]);
-  mov(rsi, qword[rsp + 8 * 2]);
-  mov(rdx, qword[rsp + 8 * 3]);
   ret();
 #else
   assert_always("Unknown platform ABI in host to guest thunk!");
