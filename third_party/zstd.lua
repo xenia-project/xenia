b56c56faee3601b4b13780b95e071e58a92c1914
Margen67 <Margen67@users.noreply.github.com>
2025-08-17 05:13:56 -0700

    [premake] Only define ZSTD_DISABLE_ASM on non-MSVC
    MSVC doesn't compile with BMI(2) by default so there's no point.
    DYNAMIC_BMI2 needs to be defined to enable it, but it's disabled since it bloats binary size.

--- third_party/zstd.lua
+++ third_party/zstd.lua
@@ -3,8 +3,9 @@ include("third_party/zstd/contrib/premake/zstd.lua")
 group("third_party")
 project("zstd")
   uuid("df336aac-f0c8-11ed-a05b-0242ac120003")
-  -- TODO: https://github.com/facebook/zstd/issues/3278
-  --defines({
-  --  "ZSTD_DISABLE_ASM",
-  --})
+  -- https://gitlab.kitware.com/cmake/cmake/-/issues/25744
+  filter({"toolset:not msc"})
+    defines({
+      "ZSTD_DISABLE_ASM",
+    })
   project_zstd("zstd/lib/")
